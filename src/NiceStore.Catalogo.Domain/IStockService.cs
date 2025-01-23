a508b03f2023bed1fec0220294d6ff489f4a6b6d
Rafael Kozar <rafael.kozar@alta-rt.com>
2025-01-23 11:15:34 -0300

    StockService is domain service

---
+++ src/NiceStore.Catalogo.Domain/IStockService.cs
@@ -0,0 +1,8 @@
+﻿namespace NiceStore.Catalog.Domain
+{
+    public interface IStockService : IDisposable
+    {
+        Task<bool> DebitStock(Guid productId, int quantity);    
+        Task<bool> ReplenishStock(Guid productId, int quantity);    
+    }
+}
---
+++ src/NiceStore.Catalogo.Domain/StockService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiceStore.Catalog.Domain
+{
+    public class StockService : IStockService
+    {
+        private readonly IProductRepository _productRepository;
+        public StockService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public async Task<bool> DebitStock(Guid productId, int quantity)
+        {
+            var product = await _productRepository.GetById(productId);
+            if (product == null) return false;
+            if(!product.HasStock(quantity)) return false;   
+            product.SubtractStock(quantity);
+            _productRepository.Update(product);
+            return await _productRepository.UnitOfWork.Commit();
+        }
+
+      
+        public async Task<bool> ReplenishStock(Guid productId, int quantity)
+        {
+            var product = await _productRepository.GetById(productId);
+            if (product == null) return false;
+            product.AddStock(quantity);
+            _productRepository.Update(product);
+            return await _productRepository.UnitOfWork.Commit();
+        }
+
+        public void Dispose()
+        {
+            _productRepository.Dispose();
+        }
+
+    }
+}

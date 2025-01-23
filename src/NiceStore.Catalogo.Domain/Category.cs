6ef97f70b107f49dd2ce1807c23e0ee004f299c8
Rafael Kozar <rafael.kozar@alta-rt.com>
2025-01-23 10:07:03 -0300

    Rename + repository

--- src/NiceStore.Catalogo.Domain/Category.cs
+++ src/NiceStore.Catalogo.Domain/Category.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NiceStore.Catalogo.Domain
+namespace NiceStore.Catalog.Domain
 {
     public class Category : Entity
     {        

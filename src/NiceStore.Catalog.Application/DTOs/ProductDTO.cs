990375df6523e5923c299476a931da9d2273af05
Rafael Kozar <rafael.kozar@alta-rt.com>
2025-02-01 18:45:54 -0300

    correction to not validade the field categories when is saving

--- src/NiceStore.Catalog.Application/DTOs/ProductDTO.cs
+++ src/NiceStore.Catalog.Application/DTOs/ProductDTO.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -49,7 +50,8 @@ namespace NiceStore.Catalog.Application.DTOs
         [Required(ErrorMessage = "The field {0} is required")]
         public int Depth { get; set; }
 
-       public IEnumerable<CategoryDTO> Categories { get; set; }
+        
+        public IEnumerable<CategoryDTO>? Categories { get; set; }
 
 
 
